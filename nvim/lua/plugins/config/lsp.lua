e94b68be243adbe8e819690e931344e61f026da3
Austin Jones <ajones53.aj@gmail.com>
2026-09-19 19:51:44 -0400

    update lazy plugins and add sourcekit lsp

--- nvim/lua/plugins/config/lsp.lua
+++ nvim/lua/plugins/config/lsp.lua
@@ -104,20 +104,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end,
 })
 
+vim.lsp.enable("sourcekit")
+
 require('mason').setup()
 require('mason-lspconfig').setup({
     ensure_installed = {
         'rust_analyzer',
-        'tsserver',
     }
-})
-
-local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-require('mason-lspconfig').setup_handlers({
-    function(server_name)
-        lspconfig[server_name].setup({
-            capabilities = lsp_capabilities,
-        })
-    end,
 })
